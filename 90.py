e8cec843a6ce0d7a0a57bf2f0720f801915efee5
pthaok6 <72274756+pthaok6@users.noreply.github.com>
2025-10-25 00:40:49 +0700

    Update 90.py

--- 90.py
+++ 90.py
@@ -26,9 +26,9 @@ def info():
 
   "leagueHot": "",
 
-  "searchBeginTime": "2025-10-24 16:00:00",
+  "searchBeginTime": "2025-10-25 16:00:00",
 
-  "searchEndTime": "2025-10-24 20:30:00",
+  "searchEndTime": "2025-10-25 20:30:00",
 
   "leagueId": []
 
@@ -208,6 +208,7 @@ while (st.json()['success']==0):
 
 
   sleep(1.5)
+
 
 
 
