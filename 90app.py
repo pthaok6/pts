098b1dc902411218e58e861221e63a84960f6c60
pthaok6 <72274756+pthaok6@users.noreply.github.com>
2025-10-30 08:31:50 +0700

    Add files via upload

---
+++ 90app.py
@@ -0,0 +1,132 @@
+import requests,os;
+
+import datetime
+
+from time import sleep;
+
+from datetime import timedelta
+
+os.system('clear')
+
+#aut=input('Author: ')
+
+aut='Bearer 768d4486-a1d8-4c30-a471-7j261ceaaf86e'
+
+#login
+
+def info():
+
+ js={
+  "pageSize": 20,
+  "pageNo": 1,
+  "guaranteed": -1,
+  "leagueHot": "",
+  "searchBeginTime": "2025-10-30 16:00:00",
+  "searchEndTime": "2025-10-30 20:00:59",
+  "leagueId": []
+}
+
+ headersinfo={
+  "DevicePlatform": "3",
+  "Authorization": "Bearer c7adead2-2245-44f4-8512-98edc06763c1",
+  "X-Requested-With": "XMLHttpRequest",
+  "appver": "2.4.0",
+  "lang": "vi_VN",
+  "user-agent": "Mozilla/5.0 (Linux; Android 12; A202SH Build/SC263; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/141.0.7390.122 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/30.0)",
+  "Content-Type": "application/json",
+  "Content-Length": "149",
+  "Host": "90trade.app",
+  "Connection": "Keep-Alive",
+  "Accept-Encoding": "gzip",
+  "Cookie": "heauacea=4cd28f4ded8d72f91d3cd0acb3b73740"
+}
+
+ return requests.post('https://90trade.app/soccer/game/page',headers=headersinfo,json=js)
+
+
+
+
+
+log=info()
+
+for records in log.json()['data']['records']:
+
+    print(f"{records['homeTeam']} vs {records['awayTeam']} - {records['date']} ID: {records['gameId']}")
+
+    print('  ')
+    
+def balance():
+  jsbl={}
+  hdbl={
+  "DevicePlatform": "3",
+  "Authorization": "Bearer c7adead2-2245-44f4-8512-98edc06763c1",
+  "X-Requested-With": "XMLHttpRequest",
+  "appver": "2.4.0",
+  "lang": "vi_VN",
+  "user-agent": "Mozilla/5.0 (Linux; Android 12; A202SH Build/SC263; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/141.0.7390.122 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/30.0)",
+  "Host": "90trade.app",
+  "Connection": "Keep-Alive",
+  "Accept-Encoding": "gzip",
+  "Cookie": "heauacea=4cd28f4ded8d72f91d3cd0acb3b73740"
+}
+  return requests.get('https://90trade.app/soccer/member/credit',json=jsbl,headers=hdbl)
+
+print('So du: ',balance().json()['data']['balance'])
+
+gameId=input('gameId: ')
+
+money=input('Money: ')
+
+
+
+def stake():
+
+ jsst={
+  "betType": 1,
+  "gameId": gameId,
+  "item1": 4,
+  "item2": 4,
+  "odds": 0.005,
+  "gameType": 1,
+  "walletType": "CASH",
+  "stake": money
+}
+ hdst={
+  "DevicePlatform": "3",
+  "Authorization": "Bearer c7adead2-2245-44f4-8512-98edc06763c1",
+  "X-Requested-With": "XMLHttpRequest",
+  "appver": "2.4.0",
+  "lang": "vi_VN",
+  "user-agent": "Mozilla/5.0 (Linux; Android 12; A202SH Build/SC263; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/141.0.7390.122 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/30.0)",
+  "Content-Type": "application/json",
+  "Content-Length": "112",
+  "Host": "90trade.app",
+  "Connection": "Keep-Alive",
+  "Accept-Encoding": "gzip",
+  "Cookie": "heauacea=4cd28f4ded8d72f91d3cd0acb3b73740"
+}
+
+ return requests.post('https://90trade.app/soccer/game/stake/create',headers=hdst,json=jsst)
+
+
+
+
+
+st=stake()
+
+print(st.status_code)
+
+#print(st.headers)
+
+print(st.text[:300])
+
+print(st.json()['message'])
+
+while (st.json()['success']==0):
+
+  st=stake()
+
+  print(st.json()['message'])
+
+
+  sleep(1.5)
