4c89d62bb13b56ff50b7b07bb03e6bf747e820b9
pthaok6 <72274756+pthaok6@users.noreply.github.com>
2025-11-03 09:14:11 +0700

    Update 90.py

--- 90.py
+++ 90.py
@@ -10,7 +10,7 @@ os.system('clear')
 
 #aut=input('Author: ')
 
-aut='Bearer 427ee252-7a26-45e9-989a-a89a33a8e987'
+aut='Bearer 2ccf967c-c73b-4f02-af55-18534265d64c'
 
 #login
 
@@ -236,6 +236,7 @@ while (st.json()['success']==0):
 
 
   sleep(1.5)
+
 
 
 
