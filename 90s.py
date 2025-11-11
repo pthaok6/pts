2332942b74ba4b16fbddf75fa497dd7ab6c74f6e
pthaok6 <72274756+pthaok6@users.noreply.github.com>
2025-11-11 09:08:15 +0700

    Update 90s.py

--- 90s.py
+++ 90s.py
@@ -3,10 +3,4 @@ os.system('clear')
 print('================================================')
 os.system('git pull')
 print('================================================')
-choose=input('1-web || 0-app => ')
-choose=1
-os.system('clear')
-if (choose==1):
- os.system('python 90.py')
-else: 
- os.system('python 90.py')
+os.system('python 90.py')
