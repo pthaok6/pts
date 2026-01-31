6147cfe7db3a343772cf22dd02bca0483c89a3d0
pthaok6 <72274756+pthaok6@users.noreply.github.com>
2026-01-31 09:42:15 +0700

    Update 90.py

--- 90.py
+++ 90.py
@@ -10,7 +10,7 @@ os.system('clear')
 
 #aut=input('Author: ')
 
-aut='Bearer 16bc71b0-3336-405a-b06a-e15342b2cd03'
+aut='Bearer dfa5d0bc-2e1a-4b9e-ac66-412cdeac99eb'
 # tạo múi giờ Việt Nam 
 vietnam_tz = timezone(timedelta(hours=7)) 
 now_vn = datetime.now(vietnam_tz)
@@ -22,7 +22,7 @@ def info():
 
 # tự động lấy ngày hiện tại
 
- begin_dt = now_vn.replace(hour=16, minute=0, second=0, microsecond=0)
+ begin_dt = now_vn.replace(hour=17, minute=0, second=0, microsecond=0)
 
  end_dt   = now_vn.replace(hour=22, minute=30, second=0, microsecond=0)
 
@@ -251,6 +251,7 @@ while (st.json()['success']==0):
      sleep(1);
   else:
    sleep(1.5)
+
 
 
 
