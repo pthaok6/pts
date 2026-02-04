b93ae98eb6cfd13bf5547a4c173116a89afb8a94
pthaok6 <72274756+pthaok6@users.noreply.github.com>
2026-02-04 10:24:01 +0700

    Update 90.py

--- 90.py
+++ 90.py
@@ -22,7 +22,7 @@ def info():
 
 # tự động lấy ngày hiện tại
 
- begin_dt = now_vn.replace(hour=17, minute=0, second=0, microsecond=0)
+ begin_dt = now_vn.replace(hour=15, minute=0, second=0, microsecond=0)
 
  end_dt   = now_vn.replace(hour=22, minute=30, second=0, microsecond=0)
 
@@ -251,6 +251,7 @@ while (st.json()['success']==0):
      sleep(1);
   else:
    sleep(1.5)
+
 
 
 
